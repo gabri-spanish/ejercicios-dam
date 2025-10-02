9e0a49df2392cca61bea46f2c16b7a17492b4e35
A715\dam1 <gabrieltortosa00@gmail.com>
2025-10-02 17:35:49 +0200

    Staged

--- ejercicios_dam/src/main/java/org/example/ratónjuguetón.java
+++ ejercicios_dam/src/main/java/org/example/ratónjuguetón.java
@@ -1,31 +1,56 @@
 package org.example;
-
 import java.util.Scanner;
 
 public class ratónjuguetón {
     public static void main(String[] args) {
-    Scanner teclado = new Scanner(System.in);
-        System.out.println("***BIENVENIDO AL RATÓN MATÓN***");
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("***BIENVENIDO AL RAT�N LAT�N***");
 
         System.out.println("Introduce tu altura: ");
-        int numero = teclado.nextInt();
+        int altura = teclado.nextInt();
 
-        if (numero == 0) {
+        if (altura <= 140) {
 
-            System.out.println("El número es cero");
+            if (altura < 0) {
 
-        } else if (numero < 0) {
+                System.out.println("�ERROR DE ALTURA, BAJA Y SUBETE DE VUELTA!");
+                System.exit(0);
 
-            System.out.println(numero + " es negativo");
+            }
 
-        } else {
-
-            System.out.println(numero + " es positivo");
+            int falta = 140 - altura;
+            System.out.println("NO PUEDES MONTAR, TE FALTAN " + falta + "cm DE ALTURA");
+            System.exit(0);
 
         }
 
-    }
+        else if (altura >= 230 ) {
 
+                System.out.println("�ERROR DE ALTURA, BAJA Y SUBETE DE VUELTA!");
+            System.exit(0);
+
+            }
+
+        System.out.println("Introduce tu peso: ");
+        int peso = teclado.nextInt();
+
+        int minimo = (altura * 2) / 8;
+        int maximo = 120;
+
+        if ( peso > maximo ) {
+
+            int sobre = peso - minimo;
+            System.out.println("LO SIENTO, PESAS " + sobre + "KG por arriba del l�mite de la atracci�n");
+            System.exit(0);
+
+        }
+
+        else {
+
+            System.out.println("Pesas " + peso + "KG. Peso m�nimo calculado: " + minimo + "KG.");
+            System.out.println("���PUEDES SUBIR, P�SALO BIEN!!!");
+
+        }
 
     }
 }
