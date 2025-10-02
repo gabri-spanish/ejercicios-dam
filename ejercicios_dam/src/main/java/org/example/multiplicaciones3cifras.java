d369b676e4c7ba6216e16a266ecf9a0d950ffa03
A715\dam1 <gabrieltortosa00@gmail.com>
2025-10-02 18:14:57 +0200

    Subo archivo para guardarlo y seguir luego

---
+++ ejercicios_dam/src/main/java/org/example/multiplicaciones3cifras.java
@@ -0,0 +1,19 @@
+package org.example;
+import java.util.Scanner;
+
+public class multiplicaciones3cifras {
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Introduce el primer número de 3 cifras: ");
+        int numpri = teclado.nextInt();
+
+        System.out.println("Introduce el segundo número de 3 cifras: ");
+        int numsec = teclado.nextInt();
+
+
+
+
+    }
+}
