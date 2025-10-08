28f75f08d87186fe71486a0f4a07837ab533c7f6
A715\dam1 <gabrieltortosa00@gmail.com>
2025-10-08 20:45:39 +0200

    Archivo finalizado

--- ejercicios_dam/src/main/java/org/example/Examen_UD1.java
+++ ejercicios_dam/src/main/java/org/example/Examen_UD1.java
@@ -2,8 +2,8 @@ package org.example;
 import java.util.Scanner;
 
 public class Examen_UD1 {
-    static class renta{
 
+    static class renta{
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
 
@@ -36,14 +36,33 @@ public class Examen_UD1 {
 class papa {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-    int cardenales = 0;
-    int votos = 0;
 
-         System.out.println("Introduce el número de cardenales");
-         cardenales = teclado.nextInt();
+    System.out.println("Introduce el número de cardenales");
+    int cardenales = teclado.nextInt();
+
+    if (cardenales < 2 || cardenales > 200) {
+
+        System.out.println("ERROR. No pueden haber menos de 2 ni más de 200 cardenales");
+        System.exit(0);
+
+    }
 
     System.out.println("Introduce el número de votos");
-    votos = teclado.nextInt();
+    int votos = teclado.nextInt();
+
+    if (cardenales < votos) {
+
+        System.out.println("ERROR. No pueden haber más votos ("+ votos + ") que cardenales ("+ cardenales + ")");
+        System.exit(0);
+
+    }
+
+        if (votos <= 0) {
+
+            System.out.println("ERROR. No pueden haber 0 votos, ¡HAY QUE VOTAR AL PAPA!");
+            System.exit(0);
+
+        }
 
     int fumata = (cardenales % votos);
 
