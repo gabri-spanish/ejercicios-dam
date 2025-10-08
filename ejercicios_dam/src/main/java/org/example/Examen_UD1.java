718146129e55aed4edfad2eb03d619eb1a53eede
A715\dam1 <gabrieltortosa00@gmail.com>
2025-10-08 20:29:36 +0200

    Terminado el ejercicio

--- ejercicios_dam/src/main/java/org/example/Examen_UD1.java
+++ ejercicios_dam/src/main/java/org/example/Examen_UD1.java
@@ -2,13 +2,12 @@ package org.example;
 import java.util.Scanner;
 
 public class Examen_UD1 {
-    public class Renta {}
-    public class Papa{}
-    Scanner teclado = new Scanner(System.in);
+    static class renta{
 
-    public void main(Renta args) {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
 
-        int importe = 0;
+        double importe = 0;
         try {
             System.out.println("Introduce el importe del resultado de tu declaración");
             importe = teclado.nextInt();
@@ -18,7 +17,7 @@ public class Examen_UD1 {
 
         }
 
-        if (0 > importe) {
+        if (importe < 0) {
 
             System.out.println("Resultado de la declaración: A DEVOLVER");
 
@@ -26,9 +25,40 @@ public class Examen_UD1 {
 
         else {
 
-            System.out.println("Resultado de la declaración: A DEVOLVER");
+            System.out.println("Resultado de la declaración: A PAGAR");
 
         }
+
+        }
+    }
+}
+
+class papa {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+    int cardenales = 0;
+    int votos = 0;
+
+         System.out.println("Introduce el número de cardenales");
+         cardenales = teclado.nextInt();
+
+    System.out.println("Introduce el número de votos");
+    votos = teclado.nextInt();
+
+    int fumata = (cardenales % votos);
+
+    if (fumata == 0 ) {
+
+        System.out.println("Fumata blanca");
+        System.out.println("HABEMUS PAPAM");
+
+    }
+
+    else {
+
+        System.out.println("Fumata negra");
+
+    }
 
     }
 }
