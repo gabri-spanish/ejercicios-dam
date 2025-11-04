4b45b2982100765788b754e208eaa5312947e945
A715\dam1 <gabrieltortosa00@gmail.com>
2025-11-04 16:50:12 +0100

    no message

---
+++ ejercicios_dam/src/main/java/tema3/vector.java
@@ -0,0 +1,13 @@
+package tema3;
+
+import java.util.Scanner;
+
+public class vector {
+    static void main() {
+
+    Scanner teclado = new Scanner(System.in);
+
+    int[] vector = {1,2,3,4,5};
+
+    }
+}
