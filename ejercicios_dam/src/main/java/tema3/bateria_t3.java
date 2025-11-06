95a23e4fa1e008cdb44ded8a6e7443be903f8426
A715\dam1 <gabrieltortosa00@gmail.com>
2025-11-06 19:29:47 +0100

    no message

--- ejercicios_dam/src/main/java/tema3/bateria_t3.java
+++ ejercicios_dam/src/main/java/tema3/bateria_t3.java
@@ -86,10 +86,10 @@ class act4 {
 
 
         Scanner teclado = new Scanner(System.in);
-        int vector[] = new int[6];
+        Random aleatorio = new Random();
+        int vector[] = new int [5];
         int filtrado[] = new int [vector.length-1];
         int borrar = 0;
-        int aux = 0;
 
         System.out.println("Crea tu array");
         for (int i=0; i<filtrado.length;i++) {
@@ -111,6 +111,95 @@ class act4 {
 
         }
         System.out.println(Arrays.toString(filtrado));
+
+    }
+}
+
+
+class act5 {
+    public static void main (String[] args) {
+        int vector[] = {1,2,3,4,5};
+        System.out.println(Arrays.toString(vector));
+        int aux = 0;
+
+        aux = vector[vector.length-1];
+
+        for (int i=vector.length-1; i>=1;i--) {
+            if (i == vector.length-1) {
+                aux = vector[i];
+                vector[i] = vector[i - 1];
+            } else {
+                vector[i] = vector[i - 1];
+            }
+        }
+
+        vector[0] = aux;
+
+        System.out.println(Arrays.toString(vector));
+
+    }
+}
+
+
+class act6 {
+    public static void main (String[] args) {
+        int vector[] = new int [5];
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Escribe tu vector");
+        boolean siono = true;
+
+        for (int i=0; i<vector.length;i++) {
+            vector[i] = teclado.nextInt();
+        }
+
+        for (int i=0; i<vector.length/2;i++) {
+            if (vector[i] != vector[vector.length-1-i]) {
+                System.out.println("No es palíndromo");
+                siono=false;
+                break;
+            }
+        }
+
+        if (siono) System.out.println("Es palíndromo");
+
+        System.out.println(Arrays.toString(vector));
+
+    }
+}
+
+class act9 {
+    public static void main(String[] args) {
+
+        String[] palabras = {"gambas", "fentanilo", "Pedri", "Osaka", "Batman"};
+        String palabraMasLarga = "";
+
+        for (String palabra : palabras) {
+            if (palabra.length() > palabraMasLarga.length()) {
+                palabraMasLarga = palabra;
+            }
+        }
+
+        System.out.println("La palabra más larga es: " + palabraMasLarga);
+    }
+}
+
+
+class act10 {
+    public static void main (String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+        String palabra[] = {"mandarina","gambas","fentanilo","pan"};
+        System.out.println("Busca la palabra");
+        char letra = teclado.next().charAt(0);
+
+        for (int i=0; i<palabra.length; i++) {
+
+            if (letra == palabra[i].charAt(0)) {
+                System.out.println(palabra[i]);
+            }
+
+        }
+
 
     }
 }
