6afe39825156bfc64dd5647a7484ca1bced365cf
A715\dam1 <gabrieltortosa00@gmail.com>
2025-11-25 20:30:31 +0100

    no message

--- ejercicios_dam/src/main/java/tema3/Practica3_ZXSpectrum.java
+++ ejercicios_dam/src/main/java/tema3/Practica3_ZXSpectrum.java
@@ -25,23 +25,33 @@ public class Practica3_ZXSpectrum {
 
         teclado.nextLine();
 
-        String matriz[][] = new String [ancho][alto];
+        String matriz[][] = new String [alto][ancho];
 
         System.out.println("Introduce (línea a línea) los colores de tu imagen para cada píxel:");
         externo:
-        for (int i=0;i<ancho;i++) {
-          if (matriz[i][0].length() > alto) {
-              break externo;
+        for (int i=0;i<matriz.length;i++) {
+            String letra = teclado.next();
+            String letrasplit[] = letra.split("");
+          if (letra.matches("[A-Oa-o]+") && letrasplit.length == ancho) {
+              for (int j = 0; j < matriz[i].length; j++) {
+                  matriz[i][j] = letrasplit[j];
+              }
+          } else {
+              System.out.println("Valor inválido");
+              i--;
           }
-            System.out.println();
         }
 
         teclado.nextLine();
 
         for (String[] fila : matriz) {
-            for (String valor : fila) {
-                System.out.println(valor + " ");
-            }
+            System.out.println(Arrays.toString(fila));
+        }
+
+        int conta = 0;
+
+        for (int i=0; i< matriz.length;i++) {
+
         }
 
     }
