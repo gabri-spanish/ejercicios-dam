40ec44dc068a3cadb9a5a8899a124aad08a6958c
A715\dam1 <gabrieltortosa00@gmail.com>
2025-12-10 20:58:59 +0100

    no message

--- ejercicios_dam/src/main/java/tema4/LoteriaDeLaPeñaAtletica.java
+++ ejercicios_dam/src/main/java/tema4/LoteriaDeLaPeñaAtletica.java
@@ -6,22 +6,36 @@ public class LoteriaDeLaPeñaAtletica {
         public static void main(String[] args) {
             Scanner teclado = new Scanner(System.in);
 
+            int limiteBoletos = 10000;
+            int limiteNumeros = 99999;
 
             System.out.println("Introduce cuántos boletos quieren los hinchas");
-            int boletos = teclado.nextInt();
+                int boletos = teclado.nextInt();
 
-            for (int i = 0; i < boletos; i++) {
-                int cantidad = teclado.nextInt();
-                int[] decimo = new int[cantidad];
+                bucle:
+                if (boletos <= limiteBoletos) {
 
-                for (int j = 0; j < cantidad; j++) {
-                    decimo[j] = teclado.nextInt();
+                    for (int i = 0; i < boletos; i++) {
+                        System.out.println("Introduce cuántos números hay en el décimo");
+                        int cantidad = teclado.nextInt();
+
+                        int[] decimo = new int[cantidad];
+
+                        System.out.println("Por último, introduce los números del décimo");
+
+                        for (int j = 0; j < cantidad; j++) {
+                            decimo[j] = teclado.nextInt();
+                        }
+
+                        int resultado = contarPar(decimo);
+
+                        System.out.println(resultado);
+                    }
+
+                } else {
+                    System.out.println("MÁXIMO SE PERMITEN 10.000 BOLETOS");
+
                 }
-
-                int resultado = contarPar(decimo);
-
-                System.out.println(resultado);
-            }
 
         }
 
